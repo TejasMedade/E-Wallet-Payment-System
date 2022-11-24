1bf5dcf0ec475c3694f93399689dfe318c234ac1
medadeshreyas <shreyasmedade06@gmail.com>
2022-11-25 01:43:16 +0530

    #Updated

--- Payment Wallet Application/src/main/java/com/masai/model/Address.java
+++ Payment Wallet Application/src/main/java/com/masai/model/Address.java
@@ -7,7 +7,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
